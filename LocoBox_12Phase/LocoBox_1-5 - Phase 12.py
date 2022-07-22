4bb4bcc09684b1c9e711580218d47bd4c81503d3
Zow Ormazabal <sormazabal@uc.cl>
2022-07-22 14:36:13 +0800

    move down lower menu

--- LocoBox_12Phase/LocoBox_1-5 - Phase 12.py
+++ LocoBox_12Phase/LocoBox_1-5 - Phase 12.py
@@ -3720,12 +3720,14 @@ if __name__ == '__main__':
 
     #tab1
 
-    canvas1 = Canvas(ParentFrame1, width=750, height=300) #, highlightbackground="red", highlightthickness=2
+    canvas1 = Canvas(ParentFrame1, width=750, height=200) #, highlightbackground="red", highlightthickness=2
     scroll1 = Scrollbar(ParentFrame1, orient=VERTICAL, command=canvas1.yview)
+    scrollx1 = Scrollbar(ParentFrame1, orient=HORIZONTAL, command=canvas1.xview)
+    scrollx1.grid(row=1, column=1, sticky='ew')
     
     canvas1.grid(row=0, column=0)
     scroll1.grid(row=0, column=1, sticky='ns')
-    canvas1.config(yscrollcommand=scroll1.set)
+    canvas1.config(yscrollcommand=scroll1.set, xscrollcommand=scrollx1.set)
     tab1 = Frame(canvas1, width=200, height=300)#, highlightbackground="black", highlightthickness=1
     tab1.bind(
     "<Configure>",
@@ -3826,29 +3828,29 @@ if __name__ == '__main__':
     status.set('Available ports: '+', '.join(map(str,openPorts)))
 
     #Entry for Port, Baud, timeout, filename to save
-    Label(text = 'Port').place(x = 40, y = 270)
-    Label(text =  'Baud rate').place(x = 363, y = 270)
-    Label(text = 'Time out').place(x= 565, y=270)
-    Label(text= 'File').place(x=40, y=310)
-    Label(text= 'Schedule').place(x=363, y=310)
+    Label(text = 'Port').place(x = 40, y = 370)
+    Label(text =  'Baud rate').place(x = 363, y = 370)
+    Label(text = 'Time out').place(x= 565, y=370)
+    Label(text= 'File').place(x=40, y=410)
+    Label(text= 'Schedule').place(x=363, y=410)
 
     port_entry = Spinbox(values=openPorts, width=25)
     port_entry.delete(0,'end')
     port_entry.insert(0,openPorts[0]) #first port is the default 
-    port_entry.place(x = 80, y = 270)
+    port_entry.place(x = 80, y = 370)
     baud_entry = Spinbox(values=(300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200), width=7)
     baud_entry.delete(0,'end')
     baud_entry.insert(0,'9600')
-    baud_entry.place(x = 440, y = 270)
+    baud_entry.place(x = 440, y = 370)
     timeout_entry = Entry(width = 4)
-    timeout_entry.place(x=635,y=270)
+    timeout_entry.place(x=635,y=370)
     timeout_entry.insert(0,'10')
     filename_entry = Entry(width = 25)
-    filename_entry.place(x=80, y=310)
+    filename_entry.place(x=80, y=410)
     date_string = time.strftime('%Y%m%d') # predefine a default filename with ISO date    
     filename_entry.insert(0,'BOX1-5-'+date_string+'.txt')
     configfilename_entry = Entry(width = 25)
-    configfilename_entry.place(x=440, y=310)
+    configfilename_entry.place(x=440, y=410)
     configfilename_entry.insert(0,'BOX1-5-sched-'+date_string+'.json')
 
     btnSave = Button(text=' Save ', command=save_conf, state='disabled')
@@ -3864,17 +3866,17 @@ if __name__ == '__main__':
 
     # button positions change depending on OS
     if sys.platform.startswith('win'):
-        btnSave.place(x=570, y=350)
-        btnRun.place(x=610, y=350)
+        btnSave.place(x=570, y=450)
+        btnRun.place(x=610, y=450)
     elif sys.platform.startswith('darwin'):
-        btnSave.place(x=685, y=350)
-        btnRun.place(x=745, y=350)
+        btnSave.place(x=685, y=450)
+        btnRun.place(x=745, y=450)
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        btnSave.place(x=650, y=350)
-        btnRun.place(x=720, y=350)
+        btnSave.place(x=650, y=450)
+        btnRun.place(x=720, y=450)
     else:
-        btnSave.place(x=635, y=350)
-        btnRun.place(x=695, y=350)
+        btnSave.place(x=635, y=450)
+        btnRun.place(x=695, y=450)
 
     row_adj = 3  # useful when a new row is added above
 
